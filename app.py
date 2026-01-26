2283e1f24cee2a8a7eb04e6285582878af9ec13c
dmitrymedv1-arch <dmitrymedv1@gmail.com>
2026-01-26 15:06:00 +0500

    app.py

--- app.py
+++ app.py
@@ -1771,7 +1771,7 @@ class CircularGauge:
         formatted_value = self._format_value(current_value)
         
         ax.text(
-            0, -0.05,
+            0, -0.15,
             formatted_value,
             ha='center',
             va='center',
@@ -2408,5 +2408,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
